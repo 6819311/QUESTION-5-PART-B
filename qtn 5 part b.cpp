533eccead7f3afe2058df58567044c2c6a4f284b
mohamed <mb1323840@gmail.com>
2024-09-22 14:05:02 +0300

    Add files via upload

---
+++ qtn 5 part b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+			
+				//PART B (i)
+    // (i) Print out your name
+    cout << "MY NAME IS MOHAMED ABDI MOHAMED ELMI" << endl;
+				
+				//PART B (ii)
+    // (ii) Print multiples of 3 from 1 to 20
+    for (int i = 1; i <= 20; i++) {
+        if (i % 3 == 0) {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
+
